dab1f5df4abe380f1587daea41a432278366fb7c
Yong Zhu <yong9981@gmail.com>
2018-12-13 06:37:22 -0700

    Debug on log

--- myfattest/src/test/java/com/github/drinkjava2/myfat/ManualTxTest.java
+++ myfattest/src/test/java/com/github/drinkjava2/myfat/ManualTxTest.java
@@ -2,6 +2,7 @@ package com.github.drinkjava2.myfat;
 
 import static com.github.drinkjava2.jdbpro.JDBPRO.param;
 import static com.github.drinkjava2.jdbpro.JDBPRO.ques;
+import static com.github.drinkjava2.jsqlbox.JSQLBOX.gctx;
 import static com.github.drinkjava2.jsqlbox.JSQLBOX.tail;
 
 import java.util.List;
@@ -27,109 +28,83 @@ import com.zaxxer.hikari.HikariDataSource;
 
 public class ManualTxTest {
 
-	SqlSessionFactory sqlSessionFactory;
-	HikariDataSource dataSource;
+	HikariDataSource ds;
 
 	@Before
 	public void init() {
 		SqlBoxContext.setGlobalNextAllowShowSql(true);
-		dataSource = new HikariDataSource();// DataSource
-		// H2 is a memory database
-		dataSource.setDriverClassName("org.h2.Driver");
-		dataSource.setJdbcUrl("jdbc:h2:mem:DBName;MODE=MYSQL;DB_CLOSE_DELAY=-1;TRACE_LEVEL_SYSTEM_OUT=0");
-		dataSource.setUsername("sa");
-		dataSource.setPassword("");
-
-		TransactionFactory transactionFactory = new JdbcTransactionFactory();
-		Environment environment = new Environment("demo", transactionFactory, dataSource);
-		Configuration configuration = new Configuration(environment);
-		configuration.addMapper(UserMapper.class);
-		sqlSessionFactory = new SqlSessionFactoryBuilder().build(configuration);
-		SqlSession session = sqlSessionFactory.openSession();
-		try {
-			String[] ddlArray = session.ctx().toDropAndCreateDDL(User.class);
-			for (String ddl : ddlArray)
-				session.ctx().quiteExecute(ddl);
-			for (int i = 1; i <= 100; i++)
-				new User().putField("firstName", "Foo" + i, "lastName", "Bar" + i, "age", i).insert(session.ctx());
-		} finally {
-			session.close();
-		}
+		ds = new HikariDataSource();// DataSource
+		ds.setDriverClassName("org.h2.Driver"); // H2 Memory database
+		ds.setJdbcUrl("jdbc:h2:mem:DBName;MODE=MYSQL;DB_CLOSE_DELAY=-1;TRACE_LEVEL_SYSTEM_OUT=0");
+		ds.setUsername("sa");
+		ds.setPassword("");
+		SqlBoxContext.setGlobalSqlBoxContext(new SqlBoxContext(ds));// 全局jSqlBox上下文
+		gctx().setName("GCTX");
+		String[] ddlArray = gctx().toDropAndCreateDDL(User.class);
+		for (String ddl : ddlArray)
+			gctx().quiteExecute(ddl);// 建空表
+		for (int i = 1; i <= 100; i++)
+			new User().putField("firstName", "Foo" + i, "lastName", "Bar" + i, "age", i).insert();// 测试数据
 	}
 
 	@After
 	public void clean() {
-		dataSource.close();
-	}
-
-	@Test
-	public void DemoTest() {
-		SqlSession session = sqlSessionFactory.openSession();
-		try {
-			// MyBatis的Session有了增强的SQL方法，jSqlBox中的pinte系列方法可以用
-			Assert.assertEquals(50, session.iQueryForLongValue("select count(*) from users where age>", ques(50),
-					" and age <= ", ques(100)));
-			List<User> users;
-			UserMapper mapper = session.getMapper(UserMapper.class);
-
-			// mapper也有增强的SQL方法了，pintea系列方法都可以用
-			Assert.assertEquals(50, mapper.eCountAll(User.class, " where age>?", param(50)));
-			Assert.assertEquals(50, mapper.countAll(" where age>", ques(50)));
-
-			// 对任意MyBatis查询都可以分页了，无侵入性
-			Helper.pagin(2, 10); // 或paginAndTrans(2,10)还可以进行跨数据库的函数翻译
-			users = mapper.getOlderThan(50);
-			Assert.assertEquals(10, users.size());
-			for (User u : users)
-				System.out.println("UserName=" + u.getFirstName() + "," + u.getLastName() + ", age=" + u.getAge());
-
-			// Tail也出现在mapper中了
-			Assert.assertEquals(100, mapper.eCountAll(Tail.class, tail("users")));
-
-			// ActiveRecord写法
-			new User().putField("firstName", "foo").insert(session.ctx());
-			Assert.assertEquals(101, new User().countAll(session.ctx()));
-		} finally {
-			session.close();
-		}
+		ds.close();
 	}
 
 	@Test
 	public void TxTest() {
+		TransactionFactory transactionFactory = new JdbcTransactionFactory();
+		Environment environment = new Environment("demo", transactionFactory, ds);
+		Configuration configuration = new Configuration(environment);
+		configuration.addMapper(UserMapper.class);
+		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(configuration);
+		// 以上4行完成MyBatis的配置
+
 		SqlSession session = null;
 		try {
 			session = sqlSessionFactory.openSession();
-			session.ctx().setAllowShowSQL(true);
+
 			// MyBatis的Session有了增强的SQL方法，jSqlBox中的pinte系列方法可以用
-			Assert.assertEquals(50, session.iQueryForLongValue("select count(*) from users where age>", ques(50),
+			Assert.assertEquals(50, session.iQueryForLongValue("select count(*) from user_tb where age>", ques(50),
 					" and age <= ", ques(200)));
-			List<User> users;
+
 			UserMapper mapper = session.getMapper(UserMapper.class);
+			
+			mapper.ctx().setAllowShowSQL(true);
+			mapper.ctx().eCountAll(User.class, " where 1<>0");// TODO: debug log did not show
+			System.out.println(mapper.ctx().getName());
+			System.out.println(gctx().getName());
 
 			// mapper也有增强的SQL方法了，pintea系列方法都可以用
-			Assert.assertEquals(50, mapper.eCountAll(User.class, " where age>?", param(50)));
-			Assert.assertEquals(50, mapper.countAll(" where age>", ques(50)));
+			Assert.assertEquals(100, mapper.eCountAll(User.class, " where age>?", param(0), " and age<", ques(200)));
 
-			// 分页
+			// ActiveRecord写法，基于当前session的源据源
+			Assert.assertEquals(100, new User().countAll(mapper.ctx()));
+
+			// 基于jSqlBox上下文的ActiveRecord，和MyBatis没有半毛钱关系
+			Assert.assertEquals(100, new User().countAll());
+
+			// 无侵入的分页
 			Helper.pagin(2, 10);
-			users = mapper.getOlderThan(50);
-			Assert.assertEquals(10, users.size());
-			for (User u : users)
-				System.out.println("UserName=" + u.getFirstName() + "," + u.getLastName() + ", age=" + u.getAge());
+			try {
+				List<User> users = mapper.getOlderThan(50);
+				Assert.assertEquals(10, users.size());
+			} finally {
+				Helper.clear();
+			}
 
-			// Tail也出现在mapper中了
-			Assert.assertEquals(100, mapper.eCountAll(Tail.class, tail("users")));
+			Assert.assertEquals(100, new User().countAll());
 
- 
-			// ActiveRecord写法
-			new User().putField("firstName", "Foo").insert(mapper.ctx());
-			new User().putField("firstName", "Bar").insert(mapper.ctx());
-			new User().putField("firstName", "Bar").insert(mapper.ctx());
-			new User().putField("firstName", "Bar").insert(mapper.ctx());
-			Assert.assertEquals(51, new User().countAll(mapper.ctx(), " where age>", ques(50)));
+			// 插入一行
+			new User().putField("firstName", "Tom").insert(mapper.ctx());
+			new Tail().putTail("FIRST_NAME", "Sam").insert(mapper.ctx(), tail("user_tb"));
+			Assert.assertEquals(102, new User().countAll(mapper.ctx()));
+			System.out.println(1 / 0);// 测试事务回滚
 		} finally {
 			session.close();
 		}
+		Assert.assertEquals(100, new User().countAll());
 	}
 
 }
--- myfattest/src/test/java/com/github/drinkjava2/myfat/entity/UserMapper.java
+++ myfattest/src/test/java/com/github/drinkjava2/myfat/entity/UserMapper.java
@@ -6,6 +6,6 @@ import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.myfat.RootMapper;
 
 public interface UserMapper extends RootMapper<User> {
-	@Select("select * from users where age>#{age}")
+	@Select("select * from user_tb where age>#{age}")
 	List<User> getOlderThan(int age);
 }
